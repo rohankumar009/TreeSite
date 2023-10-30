d08817d4d3349c0d35a536184de295109d3cc654
Rohan Kumar <96805095+rohankumar009@users.noreply.github.com>
2023-10-30 10:26:03 -0600

    Delete static directory

--- static/js/contact.js
+++
@@ -1,8 +0,0 @@
-const dropdown = document.getElementById('dropdown');
-        const dollarValue = document.getElementById('dollarValue');
-
-        dropdown.addEventListener('change', function() {
-            const selectedOption = dropdown.options[dropdown.selectedIndex];
-            const value = selectedOption.value;
-            dollarValue.value = '$' + value;
-        });
